b3ddcaa19f9dbb42f5b19ae6f2c35a08f5f0e7f5
sumit <sumit@peppersquare>
2016-10-17 11:02:46 +0530

    first commit

---
+++ www/js/custom.js
@@ -0,0 +1,8 @@
+(function($){
+  $(function(){
+    $(".button-collapse").sideNav();
+    $('.slider').slider({full_width: true});
+    $('.materialboxed').materialbox();
+    $('.modal-trigger').leanModal();
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
