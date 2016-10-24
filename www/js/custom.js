77150d17f3124aed0fad48f275af8e3adda29512
sumit <sumit@peppersquare>
2016-10-24 14:55:21 +0530

    deleted all fiels

--- www/js/custom.js
+++
@@ -1,8 +0,0 @@
-(function($){
-  $(function(){
-    $(".button-collapse").sideNav();
-    $('.slider').slider({full_width: true});
-    $('.materialboxed').materialbox();
-    $('.modal-trigger').leanModal();
-  }); // end of document ready
-})(jQuery); // end of jQuery name space
